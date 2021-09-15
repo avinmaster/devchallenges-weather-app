cea3f577907cb5f057020d8de94268bcdc49a67a
GitCodeDestroyer <oybek.odili@gmail.com>
2021-09-15 13:56:14 +0500

    Added Vuex

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
+// Packages
 import Vue from 'vue'
+import 'es6-promise/auto'
+
+// Vuex
+import store from './store'
+
+// Components
 import App from './App.vue'
-import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
+// Main instance
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    // count: 0
+  },
+  mutations: {
+    // increment(state) {
+    //   state.count++
+    // }
+  },
+  actions: {
+
+  },
+  getters: {
+
+  }
+})
