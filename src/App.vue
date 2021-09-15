aa88a5727bc73bb69dad4a13f81d5d0aaecb54e2
GitCodeDestroyer <oybek.odili@gmail.com>
2021-09-15 13:47:13 +0500

    Initialised Mainbar and Sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Mainbar></Mainbar>
+    <Sidebar></Sidebar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Mainbar from './components/Mainbar.vue'
+  import Sidebar from './components/Sidebar.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      Sidebar,
+      Mainbar
+    },
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Mainbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Mainbar</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Mainbar'
+}
+</script>
